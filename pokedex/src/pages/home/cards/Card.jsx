478bc62e07a73d82f44ddc748c0ab2c3cc5bc0e1
Xbuccio <franbuccio@gmail.com>
2023-11-14 09:04:19 -0300

    a
    a

--- pokedex/src/pages/home/cards/Card.jsx
+++ pokedex/src/pages/home/cards/Card.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import css from './card.module.scss'
 import axios from 'axios'
-import { URL_ESPECIES, URL_POKEMON } from '../../../api/apiRest.js'
+import { URL_ESPECIES, URL_POKEMON, URL_EVOLUCIONES } from '../../../api/apiRest.js'
 
 export default function Card({ card }) {
 
@@ -16,7 +16,7 @@ export default function Card({ card }) {
     }
 
     dataPokemon()
-  }, [])
+  }, [card])
 
   useEffect(() => {
     const dataEspeciePokemon = async () => {
@@ -24,11 +24,50 @@ export default function Card({ card }) {
       const URL = card.url.split('/')
       const api = await axios.get(`${URL_ESPECIES}/${URL[6]}`)
 
-      setEspeciePokemon(api.data)
+      setEspeciePokemon({
+        url_especie: api?.data?.evolution_chain,
+        data: api?.data
+      })
     }
 
     dataEspeciePokemon()
-  }, [])
+  }, [card])
+
+  useEffect(() => {
+    async function getPokemonImage(id) {
+      const response = await axios.get(`${URL_POKEMON}/${id}`);
+      return response?.data?.sprites?.other["oficial-artwork"]?.front_default;
+    }
+
+    if (especiePokemon?.url_especie) {
+
+      const getEvolution = async () => {
+        const arrayEvolutions = []
+        const URL = especiePokemon?.url_especie?.url.split('/')
+        const api = await axios.get(`${URL_EVOLUCIONES}/${URL[6]}`)
+
+        const URL2 = api?.data?.chain?.species?.url.split('/')
+        const img1 = await getPokemonImage(URL2[6])
+        console.log(img1)
+
+        arrayEvolutions.push({
+
+        })
+
+      }
+      getEvolution()
+    }
+
+
+  }, [especiePokemon])
+
+
+  let pokeId = itemPokemon?.id?.toString();
+  if (pokeId?.length === 1) {
+    pokeId = "00" + pokeId;
+  } else if (pokeId?.length === 2) {
+    pokeId = "0" + pokeId;
+  }
 
   return (
     <div className={css.card}>
@@ -37,12 +76,12 @@ export default function Card({ card }) {
         alt="pokemon"
         className={css.img_poke}
       />
-      <div className={`bg-${especiePokemon?.color?.name} ${css.sub_card}`} >
-        <strong className={css.id_card}> 011 </strong>
-        <strong className={css.name_card}> name </strong>
-        <h4 className={css.altura_card}> 10cm </h4>
-        <h4 className={css.peso_card}> peso </h4>
-        <h4 className={css.habitat_card}> habitat </h4>
+      <div className={`bg-${especiePokemon?.data?.color?.name} ${css.sub_card}`} >
+        <strong className={css.id_card}> {pokeId} </strong>
+        <strong className={css.name_card}> {itemPokemon.name} </strong>
+        <h4 className={css.altura_card}>Height: {itemPokemon.height}0 cm </h4>
+        <h4 className={css.peso_card}>Weight: {itemPokemon.weight} kg </h4>
+        <h4 className={css.habitat_card}> Habitat: {especiePokemon?.data?.habitat?.name}{" "} </h4>
 
         <div className={css.div_stats}>
           {itemPokemon?.stats?.map((sta, index) => {
@@ -54,9 +93,9 @@ export default function Card({ card }) {
           })}
         </div>
 
-        <div  className={css.div_type_color}>
+        <div className={css.div_type_color}>
           {itemPokemon?.types?.map((ti, index) => {
-            return <h6 key={index} className={`color-${ti.type.name} ${css.color_type}`}> 
+            return <h6 key={index} className={`color-${ti.type.name} ${css.color_type}`}>
               {" "}
               {ti.type.name}{" "}
             </h6>
