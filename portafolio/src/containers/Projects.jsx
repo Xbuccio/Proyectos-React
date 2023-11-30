25c18630215271ca2fcf3fc90baf107c518a2a88
Xbuccio <franbuccio@gmail.com>
2023-11-29 22:03:21 -0300

    portafolio

---
+++ portafolio/src/containers/Projects.jsx
@@ -0,0 +1,65 @@
+import "../styles/Projects.css"
+
+function Projects() {
+  return (
+    <section className="project">
+      <h1>Projects</h1>
+
+      <div className="projects-grid">
+        <div className="myCard">
+          <div className="innerCard">
+            <div className="frontSide">
+              <p className="title">FRONT SIDE</p>
+              <p>Hover Me</p>
+            </div>
+            <div className="backSide">
+              <p className="title">BACK SIDE</p>
+              <p>Leave Me</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="myCard">
+          <div className="innerCard">
+            <div className="frontSide">
+              <p className="title">FRONT SIDE</p>
+              <p>Hover Me</p>
+            </div>
+            <div className="backSide">
+              <p className="title">BACK SIDE</p>
+              <p>Leave Me</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="myCard">
+          <div className="innerCard">
+            <div className="frontSide">
+              <p className="title">FRONT SIDE</p>
+              <p>Hover Me</p>
+            </div>
+            <div className="backSide">
+              <p className="title">BACK SIDE</p>
+              <p>Leave Me</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="myCard">
+          <div className="innerCard">
+            <div className="frontSide">
+              <p className="title">FRONT SIDE</p>
+              <p>Hover Me</p>
+            </div>
+            <div className="backSide">
+              <p className="title">BACK SIDE</p>
+              <p>Leave Me</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
