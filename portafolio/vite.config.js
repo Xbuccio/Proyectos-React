3a265a8d4a46d4a63f07b442a6141065fcb82cec
Xbuccio <franbuccio@gmail.com>
2023-12-01 16:24:02 -0300

    a

--- portafolio/vite.config.js
+++ portafolio/vite.config.js
@@ -1,10 +1,15 @@
 // portafolio/vite.config.js
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Proyectos-React/', // ajusta la base según tu estructura en GitHub Pages
-})
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx', // ajusta la ruta según tu estructura
+    },
+  },
+});
