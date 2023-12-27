6285f63be7672deeaa4fda25ec71bc9406f7b02e
Xbuccio <franbuccio@gmail.com>
2023-12-27 18:30:46 -0300

    27/12

--- lol-data/src/App.jsx
+++ lol-data/src/App.jsx
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Asegúrate de importar Routes
-
-import Champions from "./components/Champions";
-import ChampionDetails from "./components/ChampionDetails";
-import "./styles/App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Champions from './components/Champions';
+import ChampionDetails from './components/ChampionDetails';
+import './styles/App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Champions />} />
-        <Route path="/campeon/:id" element={<ChampionDetails />} />
+        <Route path="/:id" element={<ChampionDetails />} />
       </Routes>
     </Router>
   );
