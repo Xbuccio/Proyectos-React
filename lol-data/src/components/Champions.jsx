86b4846faa30687a18823d0c51ce659623de05b9
Xbuccio <franbuccio@gmail.com>
2023-12-27 17:32:48 -0300

    27/12
    Ya se puede navegar

--- lol-data/src/components/Champions.jsx
+++ lol-data/src/components/Champions.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { URL_CHAMPIONS, URL_CHAMPIONS_IMAGE } from '../api/apiRest';
+import ChampionDetails from './ChampionDetails'; // 
 import "../styles/Champions.css"
-//import ChampionDetails from './ChampionDetails'; // Asegúrate de tener la ruta correcta
 
 const Champions = () => {
   const [championData, setChampionData] = useState(null);
@@ -26,25 +27,35 @@ const Champions = () => {
     fetchData();
   }, []);
 
+
+
   return (
     <>
-    <h1>sanjkafhukaf</h1>
+      <h1>sanjkafhukaf</h1>
       {championData ? (
         <div className='champions-grid'>
+
           {Object.values(championData.data).slice(0, 50).map((champion) => (
-            <div key={champion.id} className='champion-item'>
-              <img src={URL_CHAMPIONS_IMAGE + champion.id + '_0.jpg'} alt="" className='champions-image' />
-              <h3>{champion.name}</h3>
-              {/* <ChampionDetails championID={champion.id} /> */}
-            </div>
+            <Link to={`/${champion.id}`} key={champion.id} className='a'>
+              <div className='champion-item'>
+                <div className='champion-img-container'>
+                  <img src={URL_CHAMPIONS_IMAGE + champion.id + '_0.jpg'} alt="" className='champions-image' />
+                </div>
+                <h3>{champion.name.toUpperCase()}</h3>
+              </div>
+            </Link>
           ))}
+
         </div>
       ) : (
         <p>Loading...</p>
       )}
+      {championData && championData.data && championData.keys && (
+        <ChampionDetails championID={championData.data[championData.keys[0]].id} />
+      )}
     </>
   );
-  
+
 };
 
 export default Champions;
