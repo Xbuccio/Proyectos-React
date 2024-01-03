f8fdcba23f11f844bbebbbbf7db4105e03919ecb
Xbuccio <franbuccio@gmail.com>
2024-01-03 13:18:56 -0300

    3/1

--- lol-data/src/containers/Nav.jsx
+++ lol-data/src/containers/Nav.jsx
@@ -3,9 +3,19 @@ import '../styles/Nav.css'
 const Nav = () => {
   return (
     <nav>
-      <div className="nav"></div>
-    </nav>
-  )
-}
+      <div className="nav">
+        {/* Botón de Inicio */}
+        <button className="nav-button">Inicio</button>
 
-export default Nav
+        {/* Elementos de relleno */}
+        <div className="nav-fillers">
+          <a href="#seccion1">Sección 1</a>
+          <a href="#seccion2">Sección 2</a>
+          <a href="#seccion3">Sección 3</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
