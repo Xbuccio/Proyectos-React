6f1daa87aaf23f0deb615a0feeb56ad66f4594ce
Xbuccio <franbuccio@gmail.com>
2024-01-18 22:22:06 -0300

    18/1
    prueba de API

--- youtubeMusic/src/contain/Layout.jsx
+++ youtubeMusic/src/contain/Layout.jsx
@@ -1,11 +1,13 @@
 
 import Tags from "../components/Tags"
+import MusicSearch from "../components/ejemplo"
 import "../styles/Layout.css"
 
 const Layout = ({ open }) => {
   return (
     <section className={`layout ${open ? 'layout-close' : 'layout-open'}`} >
       <Tags />
+      <MusicSearch />
     </section>
   )
 }
